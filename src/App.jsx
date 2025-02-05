5879d884d69fb0d7af84b0fd3aca674bb150cfea
Daniel_Pickering <danielm.pickering@gmail.com>
2025-02-05 01:11:18 -0800

    Add debug controls with leva

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
 import { Suspense, useState } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
-import { Html, Preload, OrbitControls } from '@react-three/drei'
+import { Html, Preload, OrbitControls, TransformControls, PivotControls } from '@react-three/drei'
+import { useControls } from 'leva'
 
 const store = [
   { name: 'one', color: 'lightpink', position: [-5.2, -2, -15], url: 'assets/test.jpg', link: 1 },
@@ -10,24 +11,36 @@ const store = [
 ]
 
 function Dome({ name, position, texture, onClick }) {
+  // Debug UI for position
+  const { meshPosition } = useControls({
+    meshPosition: {
+      value: position,
+      step: 0.1,
+    }
+  })
+
   return (
     <group>
       <mesh>
         <sphereGeometry args={[500, 60, 40]} />
         <meshBasicMaterial map={texture} side={THREE.BackSide} />
       </mesh>
-      <mesh position={position}>
-        <sphereGeometry args={[1.25, 32, 32]} />
-        <meshBasicMaterial color="white" />
-        <Html
-          position={[0, 0, 0]}
-          center
-          transform
-          sprite
-          distanceFactor={10}>
-          <button onClick={onClick}>{name}</button>
-        </Html>
-      </mesh>
+
+      {/* Add TransformControls around the clickable mesh */}
+      <TransformControls mode="translate">
+        <mesh position={meshPosition}>
+          <sphereGeometry args={[1.25, 32, 32]} />
+          <meshBasicMaterial color="white" />
+          <Html
+            position={[0, 0, 0]}
+            center
+            transform
+            sprite
+            distanceFactor={10}>
+            <button onClick={onClick}>{name}</button>
+          </Html>
+        </mesh>
+      </TransformControls>
     </group>
   )
 }
